2f17e06f34fdac0dafceb250aa52c09db32fcf0a
RakeShrestha <orakeshstha7@gmail.com>
2024-12-30 08:17:39 +0545

    Initial Commit

---
+++ HealthySleep/HealthySleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthySleepApp.swift
+//  HealthySleep
+//
+//  Created by Rakesh Shrestha on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct HealthySleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
